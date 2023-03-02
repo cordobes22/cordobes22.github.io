6be17749ab7a31c4f1daa861061af14421dd2874
cordobes22 <126739839+cordobes22@users.noreply.github.com>
2023-03-02 00:29:58 -0300

    Update cv.js

--- cv.js
+++ cv.js
@@ -2,6 +2,7 @@ document.getElementById("cambiar_color_pagina").addEventListener("click", functi
     document.body.style.backgroundColor = "yellow";
 });
 
+
 document.getElementById("volver_al_color_original").addEventListener("click", function(){
     document.body.style.backgroundColor = "white";
 });
